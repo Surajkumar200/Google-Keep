b7254974146ac9a955cfce84518f566f9029ef10
Suraj Kumar Shaw <surajshaw7778@gmail.com>
2024-02-04 08:51:27 +0530

    first commit

---
+++ googal-keep/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './components/Home';
+import DataProvider from './context/DataProvider';
+
+function App() {
+  return (
+    <DataProvider>
+      <Home />
+    </DataProvider>
+  );
+}
+
+export default App;
